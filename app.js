2add47eca9f388a06094f99ab258a69bca6d5caf
[Sergi Bosque Ródenas] <[sbosquero@uoc.edu]>
2024-09-07 14:12:21 +0200

    dB conection and model creation

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const feedRoutes = require('./routes/feed');
+
+const MONGODB_URI = 'mongodb+srv://srgibosque:NNQ3XHX3%40!8Nyrn@cluster0.oyxb5.mongodb.net/shop?w=majority&appName=Cluster0'
 
 const app = express();
 
@@ -12,10 +15,16 @@ app.use(bodyParser.json()); // application/json
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); 
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+mongoose
+  .connect(MONGODB_URI)
+  .then(result => {
+    console.log('Connected');
+    app.listen(8080);
+  })
+  .catch(err => console.error(err));
